87df75d9a87624d16fef59ee94fb9b10b1bb3411
indardjaya <indarjaya.id@gmail.com>
2022-11-25 20:02:22 +0700

    add footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ height: 100, color: "gray", marginTop: 30, marginLeft: 30, borderTop: "gray" }}>
+      <h5>Petik.com Tempat Pesan Tiket Pesawat Mudah dan Nyaman Kapan Dan Di mana pun</h5>
+    </div>
+  );
+}
+
+export default Footer;
