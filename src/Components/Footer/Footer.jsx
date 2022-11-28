95fa3728e1a12cb775bd496ca5d95cfadd4aab16
indar jaya <indarjaya.id@gmail.com>
2022-11-28 18:46:48 +0700

    add theme MUI

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <div style={{ height: 100, color: "gray", marginTop: 30, marginLeft: 30, borderTop: "gray" }}>
-      <h5>Petik.com Tempat Pesan Tiket Pesawat Mudah dan Nyaman Kapan Dan Di mana pun</h5>
+    <div style={{ height: 100, color: "gray", marginTop: 100, borderTop: "solid 1px gray" }}>
+      <h5 style={{ marginTop: 30, textAlign: "center" }}>Petik.com Tempat Pesan Tiket Pesawat Mudah dan Nyaman Kapan Dan Di mana pun</h5>
     </div>
   );
 }
