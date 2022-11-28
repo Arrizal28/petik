00bc35cd9b73049c6aa7e88ec89f78ccec2b02a5
IbnuFA <60996611+IbnuFA@users.noreply.github.com>
2022-11-28 11:01:25 +0700

    add login and register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login"
+import Register from "./Pages/Register/Register";
 import 'antd/dist/reset.css'
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
       </Routes>
     </BrowserRouter>
   );
