3548e7b768a046378e0466856dd4ea76cb4920e2
Arrizal28 <101174338+Arrizal28@users.noreply.github.com>
2022-11-28 08:39:37 +0700

    add header panel components

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Panel from "../../Components/Panel/Panel";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Panel />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
