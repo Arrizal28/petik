c0c65410f1481d46fad93b2dc0ebd3589525d378
Arrizal28 <2010631170088@student.unsika.ac.id>
2022-12-27 13:17:02 +0700

    tes web socket

--- src/Pages/Profile/Notifications.jsx
+++ src/Pages/Profile/Notifications.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Avatar, Grid } from "@mui/material";
 import NavBar from "../../Components/Navbar/Navbar";
 import Box from "@mui/material/Box";
@@ -7,14 +7,39 @@ import TopPanels from "../../Components/Header/TopPanel";
 import { CardContainer, CardNotif } from "../../Styled/MUI/TransactionStyle";
 import { FontBlue, FontNotif, FontP } from "../../Styled/ComponentUI/Styles";
 import { Col, Row } from "react-bootstrap";
+import { io } from "socket.io-client";
+import { useDispatch, useSelector } from "react-redux";
+import { notif } from "../../Redux/Actions/userAction";
+const socket = io(process.env.REACT_APP_SOCKET_API);
 
 function Notifications() {
+  const dispatch = useDispatch();
+  const { wai } = useSelector((state) => state.auth);
+  useEffect(() => {
+    socket.on("connect", () => {});
+    socket.on(`NOTIFICATIONS-${wai?.data?.id}`, (data) => {
+      dispatch(notif(data));
+      console.log(data);
+    });
+  }, [dispatch]);
+
   return (
     <>
-      <NavBar />
+      <NavBar socket={socket} wai={wai} />
       <TopPanels />
-      <Box sx={{ flexGrow: 1, justifyContent: "center", maxWidth: 1024, margin: "auto" }}>
-        <Grid container spacing={2} sx={{ flexGrow: 1, justifyContent: "center" }}>
+      <Box
+        sx={{
+          flexGrow: 1,
+          justifyContent: "center",
+          maxWidth: 1024,
+          margin: "auto",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{ flexGrow: 1, justifyContent: "center" }}
+        >
           <Grid item xs={9}>
             <Grid>
               <CardContainer variant="outlined">
@@ -23,7 +48,14 @@ function Notifications() {
             </Grid>
             <Grid>
               <CardNotif elevation={1}>
-                <Box style={{ justifyContent: "center", textAlign: "left", alignItems: "center", marginTop: 25 }}>
+                <Box
+                  style={{
+                    justifyContent: "center",
+                    textAlign: "left",
+                    alignItems: "center",
+                    marginTop: 25,
+                  }}
+                >
                   {/* <Col xs={1} style={{ justifyContent: "center", textAlign: "left", marginLeft: 20 }}> */}
                   <Avatar fontSize="small" />
                   {/* </Col>
@@ -38,14 +70,45 @@ function Notifications() {
             </Grid>
             <Grid>
               <CardNotif elevation={1}>
-                <Row style={{ justifyContent: "center", display: "flex", textAlign: "left", alignItems: "center", marginTop: 25, cursor: "pointer" }}>
-                  <Col xs={1} style={{ justifyContent: "center", textAlign: "left", marginLeft: 20 }}>
+                <Row
+                  style={{
+                    justifyContent: "center",
+                    display: "flex",
+                    textAlign: "left",
+                    alignItems: "center",
+                    marginTop: 25,
+                    cursor: "pointer",
+                  }}
+                >
+                  <Col
+                    xs={1}
+                    style={{
+                      justifyContent: "center",
+                      textAlign: "left",
+                      marginLeft: 20,
+                    }}
+                  >
                     <Avatar fontSize="small" />
                   </Col>
-                  <Col xs={1} style={{ justifyContent: "center", textAlign: "left", display: "flex" }}>
+                  <Col
+                    xs={1}
+                    style={{
+                      justifyContent: "center",
+                      textAlign: "left",
+                      display: "flex",
+                    }}
+                  >
                     <FontP>Lorem ipsum</FontP>
                   </Col>
-                  <Col xs={2} style={{ justifyContent: "center", textAlign: "right", marginLeft: 20, cursor: "pointer" }}>
+                  <Col
+                    xs={2}
+                    style={{
+                      justifyContent: "center",
+                      textAlign: "right",
+                      marginLeft: 20,
+                      cursor: "pointer",
+                    }}
+                  >
                     <FontBlue>Mark Read</FontBlue>
                   </Col>
                 </Row>
--- src/Pages/Transaction/Transaction.jsx
+++ src/Pages/Transaction/Transaction.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid } from "@mui/material";
 import NavBar from "../../Components/Navbar/Navbar";
 import Box from "@mui/material/Box";
@@ -6,23 +6,52 @@ import Footer from "../../Components/Footer/Footer";
 import TopPanels from "../../Components/Header/TopPanel";
 import { CardContainer } from "../../Styled/MUI/TransactionStyle";
 import { FontNotif } from "../../Styled/ComponentUI/Styles";
+import { getListBooking } from "../../Redux/Actions/bookingAction";
+import { useDispatch, useSelector } from "react-redux";
 import Orders from "./Orders";
 
 function Transaction() {
+  const dispatch = useDispatch();
+
+  const { listbooking } = useSelector((state) => state.booking);
+
+  useEffect(() => {
+    dispatch(getListBooking());
+  }, [dispatch]);
+
   return (
     <>
       <NavBar />
       <TopPanels />
-      <Box sx={{ flexGrow: 1, justifyContent: "center", maxWidth: 1024, margin: "auto" }}>
-        <Grid container spacing={2} sx={{ flexGrow: 1, justifyContent: "center" }}>
+
+      <Box
+        sx={{
+          flexGrow: 1,
+          justifyContent: "center",
+          maxWidth: 1024,
+          margin: "auto",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{ flexGrow: 1, justifyContent: "center" }}
+        >
           <Grid item xs={10}>
             <CardContainer variant="outlined">
               <FontNotif>Transaction</FontNotif>
             </CardContainer>
-            <Orders />
+            {listbooking?.data?.map((item) => {
+              return (
+                <>
+                  <Orders item={item} key={item.id} />
+                </>
+              );
+            })}
           </Grid>
         </Grid>
       </Box>
+
       <Footer />
     </>
   );
