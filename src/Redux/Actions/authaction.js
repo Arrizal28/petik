41d0e2170bbc30b1401229fd7deac3feef63e86a
Arrizal28 <101174338+Arrizal28@users.noreply.github.com>
2023-01-04 13:01:27 +0700

    fix error change password (#81)

--- src/Redux/Actions/authaction.js
+++ src/Redux/Actions/authaction.js
@@ -133,8 +133,8 @@ export const resetpassword = (data, token) => async (dispatch) => {
 
 export const changePassword = (data) => async (dispatch) => {
   try {
-    const result = await axios.post(
-      `${process.env.REACT_APP_AUTH_API}/auth/forgot-password`,
+    const result = await axios.put(
+      `${process.env.REACT_APP_AUTH_API}/auth/change-password`,
       data
     );
     if (result.data.status) {
